935d5631a3053313ce53d5426cf425099b8bcbb3
endi <endifranco12@gmail.com>
2022-12-22 11:50:33 -0500

    Primera version del perfil del usuario

--- inverTech/src/app/app.module.ts
+++ inverTech/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderuserinfoComponent } from './headeruserinfo/headeruserinfo.component';
+import { MainuserinfoComponent } from './mainuserinfo/mainuserinfo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderuserinfoComponent,
+    MainuserinfoComponent
   ],
   imports: [
     BrowserModule,
---
+++ inverTech/src/app/mainuserinfo/mainuserinfo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainuserinfo',
+  templateUrl: './mainuserinfo.component.html',
+  styleUrls: ['./mainuserinfo.component.css']
+})
+export class MainuserinfoComponent {
+
+}
+
