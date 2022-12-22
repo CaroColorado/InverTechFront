33b5f49fbbe363e8b34b22d7bd691427abc49d52
lsalinasp <117488134+lsalinasp@users.noreply.github.com>
2022-12-22 17:33:08 +0000

    se agrega el learning path

--- inverTech/src/app/app.module.ts
+++ inverTech/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderLearningComponent } from './header-learning/header-learning.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderLearningComponent
   ],
   imports: [
     BrowserModule,
