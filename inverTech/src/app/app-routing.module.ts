2545f45812247d31f73b975ce6dcd4909df43bd6
lsalinasp <117488134+lsalinasp@users.noreply.github.com>
2022-12-22 21:49:59 +0000

    learning space en angular

--- inverTech/src/app/app-routing.module.ts
+++ inverTech/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LearningPathComponent } from './LearningPath/learning-path.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'learning', pathMatch:'full'},
+  {path: 'learning', component: LearningPathComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- inverTech/src/app/app.module.ts
+++ inverTech/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderLearningComponent } from './header-learning/header-learning.component';
+import { LearningPathComponent } from './LearningPath/learning-path.component';
+import { LearningheaderComponent } from './LearningPath/learningheader/learningheader.component';
+import { LearningsectionComponent } from './LearningPath/learningsection/learningsection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderLearningComponent
+    LearningPathComponent,
+    LearningheaderComponent,
+    LearningsectionComponent
   ],
   imports: [
     BrowserModule,
