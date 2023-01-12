4e603839c0963370576a3f135fba26c770b718cc
lsalinasp <117488134+lsalinasp@users.noreply.github.com>
2023-01-12 17:25:34 +0000

    Adding changes to feedback

--- inverTech/src/app/app-routing.module.ts
+++ inverTech/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { UserInfoComponent } from './user-info/user-info.component';
 import { MainPageComponent } from './mainPage/main-page.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { FeedbackComponent } from './feedback/feedback.component';
 
 const routes: Routes = [
   //aqui se indican las rutas
@@ -16,7 +17,9 @@ const routes: Routes = [
   { path: 'learning', component: LearningPathComponent },
   { path: 'userinfo', component: UserInfoComponent},
   { path: 'login', component: LoginComponent},
-  { path: 'register', component: RegisterComponent}
+  { path: 'register', component: RegisterComponent},
+  { path: 'feedback', component: FeedbackComponent}
+
 ];
 
 @NgModule({
