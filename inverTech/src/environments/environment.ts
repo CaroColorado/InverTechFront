b076661723395ebe3c2fe66b587f8f93702c0cc4
root <root@DESKTOP-V1AO4B2.localdomain>
2023-05-17 10:01:17 -0500

    firebase modules

---
+++ inverTech/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBS4EC4qm37O3g9u3gFKRQqhUZo9EkicY8',
+    authDomain: 'nodo-b381b.firebaseapp.com',
+    projectId: 'nodo-b381b',
+    storageBucket: 'nodo-b381b.appspot.com',
+    messagingSenderId: '824085973311',
+    appId: '1:824085973311:web:732e7294a3e36d38ce6b8a'
+  }
+};
